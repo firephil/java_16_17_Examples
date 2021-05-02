ebe1027deff7cd9eed0446e75cc12ce2fbdb3eac
firephil <philip@firesoft.gr>
2021-05-03 02:14:10 +0300

    Initial Commit

---
+++ src/main/java/fire/java_16_17_examples/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fire.java_16_17_examples;
+
+// Run multiple examples
+public class Main {
+        
+    public static void main(String[] args){
+    
+        RecordExamples.run();
+    
+    }
+}
