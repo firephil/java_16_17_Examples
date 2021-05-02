40cb0ac70aee5ecc38a98694dd09340fb50dc890
firephil <philip@firesoft.gr>
2021-05-03 02:18:17 +0300

    Code Commit

--- src/main/java/java_16_17/Main.java
+++ src/main/java/java_16_17/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fire.java_16_17_examples;
+package java_16_17;
 
 // Run multiple examples
 public class Main {
